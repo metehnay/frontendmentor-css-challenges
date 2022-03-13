7a09abdfe3df3b9f3efa09953ddfffbef2981f4a
metehnay <oguzwst@gmail.com>
2022-03-13 23:21:50 +0300

    fix

--- advice-generator/src/App.js
+++ advice-generator/src/App.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [advice, setAdvice] = useState([]);
+  const [loading, setLoading] = useState(false);
   const getData = () => {
     fetch("https://api.adviceslip.com/advice")
       .then((resp) => resp.json())
@@ -20,11 +21,22 @@ function App() {
         <div className="container-md">
           <div className="card">
             <p className="neo">ADVICE #{advice.id}</p>
-            <h2>{advice.advice}</h2>
+            <h2>
+              {loading
+                ? `${advice.advice}`
+                : `
+                  "It is easy to sit up and take notice, what's difficult is
+                  getting up and taking action."
+                  `}
+            </h2>
             <img src={divider} className="divider" />
           </div>
           <div className="dice-background">
-            <img src={dice} className="dice" onClick={getData} />
+            <img
+              src={dice}
+              className="dice"
+              onClick={() => (getData(), setLoading(true))}
+            />
           </div>
         </div>
       </div>
